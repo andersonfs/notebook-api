fa1524e112005b81f7a0b11f3a73a13bf67638c4
Anderson Firmino <anderson.firmino@m4u.com.br>
2018-09-19 18:02:38 -0300

    Added Address model.

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -7,8 +7,10 @@ class Contact < ApplicationRecord
   # Associations
   belongs_to :kind
   has_many :phones
+  has_one :address
 
   accepts_nested_attributes_for :phones, allow_destroy: true
+  accepts_nested_attributes_for :address
 
   def birthdate_br
     I18n.l(birthdate) unless birthdate.blank?
