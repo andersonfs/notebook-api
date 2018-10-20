36fdd4ee12c663ce27832dabc8750247531060b5
Anderson Firmino <anderson.firmino@m4u.com.br>
2018-10-20 13:42:29 -0300

    Added http authenticate methods.

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,11 @@ Rails.application.routes.draw do
     resource :kind, only: [:show]
     resource :kind, only: [:show], path: 'relationships/kind'
 
-    resource :phones, only: [:show]
+    resource :phones, only: [:show]    
     resource :phones, only: [:show], path: 'relationships/phones'
+    
+    resource :phone, only: [:update, :create, :destroy]
+    resource :phone, only: [:update, :create, :destroy], path: 'relationships/phone'
 
     resource :address, only: [:show, :update, :create, :destroy]
     resource :address, only: [:show, :update, :create, :destroy], path: 'relationships/address'
