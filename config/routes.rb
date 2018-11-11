7ed013c629865f12e2b235a7b477f5384bb45bc1
Anderson Firmino <anderson.firmino@m4u.com.br>
2018-11-10 23:47:25 -0200

    Added API versioning by contexto path.

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,24 @@ Rails.application.routes.draw do
   resources :kinds
   resources :auths, only: [:create]
 
-  constraints subdomain: 'v1' do
-    scope module: 'v1' do
-      resources :contacts do
+  api_version(:module => "V1", :path => {:value => "v1"}) do
+    resources :contacts do
+      resource :kind, only: [:show]
+      resource :kind, only: [:show], path: 'relationships/kind'
+
+      resource :phones, only: [:show]
+      resource :phones, only: [:show], path: 'relationships/phones'
+
+      resource :phone, only: [:update, :create, :destroy]
+      resource :phone, only: [:update, :create, :destroy], path: 'relationships/phone'
+
+      resource :address, only: [:show, :update, :create, :destroy]
+      resource :address, only: [:show, :update, :create, :destroy], path: 'relationships/address'
+    end
+  end
+
+  api_version(:module => "V2", :path => {:value => "v2"}) do
+    resources :contacts do
         resource :kind, only: [:show]
         resource :kind, only: [:show], path: 'relationships/kind'
 
@@ -19,25 +34,6 @@ Rails.application.routes.draw do
 
         resource :address, only: [:show, :update, :create, :destroy]
         resource :address, only: [:show, :update, :create, :destroy], path: 'relationships/address'
-      end
-    end
-  end
-
-  constraints subdomain: 'v2' do
-    scope module: 'v2' do
-      resources :contacts do
-          resource :kind, only: [:show]
-          resource :kind, only: [:show], path: 'relationships/kind'
-
-          resource :phones, only: [:show]
-          resource :phones, only: [:show], path: 'relationships/phones'
-
-          resource :phone, only: [:update, :create, :destroy]
-          resource :phone, only: [:update, :create, :destroy], path: 'relationships/phone'
-
-          resource :address, only: [:show, :update, :create, :destroy]
-          resource :address, only: [:show, :update, :create, :destroy], path: 'relationships/address'
-      end
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
